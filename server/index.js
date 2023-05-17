74fcabf225af8a6df1071e06d398fab18e949c75
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-17 17:13:54 +0530

    Connect to mongoDB & enable the exchange of JSON

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const routes = require("./routes/GetTest");
+const routes = require("./routes/authRoutes");
 const app = express();
 const PORT = 5000;
+const connectDB = require("./Database/db");
+connectDB();
+
+
+// For handling JSON data
+app.use(express.json());
 
 //Registering all Routes
-app.use("/", routes);
+app.use("/auth", routes);
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
