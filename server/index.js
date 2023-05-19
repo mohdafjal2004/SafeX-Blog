bd5ea8f8b2ca51314a5c59c2aa0a05d7ddc9b0b0
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-19 09:23:02 +0530

    Enabled CORS

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const routes = require("./routes/authRoutes");
+const connectDB = require("./Database/db");
+const cors = require("cors")
+
 const app = express();
 const PORT = 5000;
-const connectDB = require("./Database/db");
 connectDB();
-
 
 // For handling JSON data
 app.use(express.json());
 
+// for handling CORS
+app.use(cors());
+
 //Registering all Routes
 app.use("/auth", routes);
-
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
