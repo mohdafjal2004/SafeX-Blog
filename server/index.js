0f4616f71b15da21022cfd1c4b782fb55e78bcf7
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-23 08:58:19 +0530

    Imported All Routes

--- server/index.js
+++ server/index.js
@@ -1,20 +1,30 @@
 const express = require("express");
-const routes = require("./routes/authRoutes");
+const Authroutes = require("./routes/authRoutes");
+const Blogroutes = require("./routes/blogRoutes");
 const connectDB = require("./Database/db");
-const cors = require("cors")
+const cors = require("cors");
+const Filerouter = require("./routes/imageRoutes");
 
 const app = express();
 const PORT = 5000;
 connectDB();
 
 // For handling JSON data
-app.use(express.json());
+app.use(express.json({ limit: "0" }));
+app.use(express.urlencoded({ extended: true, limit: "0" }));
 
-// for handling CORS
+// for handling CORS 
 app.use(cors());
 
+app.use(function (err, req, res, next) {
+  console.log("This is the invalid field ->", err.field);
+  next(err);
+});
+
 //Registering all Routes
-app.use("/auth", routes);
+app.use("/auth", Authroutes);
+app.use("/blog", Blogroutes);
+app.use("/file", Filerouter);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
