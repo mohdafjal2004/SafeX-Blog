24f6fdf7762d74db8a8894f19b83b7e1f6236730
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-26 06:51:20 +0530

    Increased the limit of multer to handle more files

--- server/middleware/multerMiddleware.js
+++ server/middleware/multerMiddleware.js
@@ -5,7 +5,10 @@ const imagestorage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single("image");
 
 
-const imageupload = multer({ storage: imagestorage }).fields([
+const imageupload = multer({
+  storage: imagestorage,
+  limits: { fieldSize: 25 * 1024 * 1024 },
+}).fields([
   { name: "cover", maxCount: 1 },
   { name: "imageFile", maxCount: 1 },
 ]);
