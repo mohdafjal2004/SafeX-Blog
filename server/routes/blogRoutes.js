2339ff718c5249d5417c5a7cb4f607e1028dd66c
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-26 06:50:06 +0530

    Delete and Update Blog in Backend

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { addBlog, getAllBlogs } = require("../controllers/blogController");
-const {  imageupload } = require("../middleware/multerMiddleware");
+const {
+  addBlog,
+  getAllBlogs,
+  getBlog,
+  deleteBlog,
+  updateBlog,
+} = require("../controllers/blogController");
+const { imageupload } = require("../middleware/multerMiddleware");
 const Blogrouter = express.Router();
 
 Blogrouter.post("/post", imageupload, addBlog);
-Blogrouter.get("/getAll", imageupload, getAllBlogs);
+Blogrouter.get("/getAll", getAllBlogs);
+Blogrouter.get("/getById/:id", getBlog);
+Blogrouter.delete("/deleteById/:id", deleteBlog);
+Blogrouter.put("/UpdateById/:id",imageupload,  updateBlog);
 module.exports = Blogrouter;
