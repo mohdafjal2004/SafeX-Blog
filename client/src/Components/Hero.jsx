9f3f2626791d2e09c1ff783381117a610754189e
Mohd Afjal <mohdafjalgci2018@gmail.com>
2023-05-28 10:51:27 +0530

    Hero component

---
+++ client/src/Components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Styles/Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="container">
+      <h1 className="title">
+        SAFE<span className="x">X</span>BLOG
+      </h1>
+      <p className="para">
+        Discover captivating content, connect with fellow bloggers, and unleash
+        your creativity. Join our secure platform and dive into a world of
+        inspiring stories, insightful discussions, and limitless possibilities.
+        Start sharing your voice and be part of our vibrant blogging community
+        today!
+      </p>
+
+      <p className="wlcmmsg">Happy Blogging !!</p>
+    </div>
+  );
+};
+
+export default Hero;
